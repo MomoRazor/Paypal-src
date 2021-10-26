b7dc852084069290fc7081d89d325a4edc523c60
MomoRazor <maurovic.cachia@gmail.com>
2021-10-26 19:46:30 +0200

    Bouncer Fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { SERVICE_PROVIDER_DOMAINS } from './enviornment';
 export const bouncer = (req: Request, res: Response, next: NextFunction) => {
     if (SERVICE_PROVIDER_DOMAINS) {
         const verifiedDomains = SERVICE_PROVIDER_DOMAINS.split(',');
-        if (verifiedDomains.includes(req.hostname)) {
+        if (verifiedDomains.includes(req.header('Origin') || '')) {
             next();
         } else {
             res.status(401).end('Locked to specific Service Provider');
